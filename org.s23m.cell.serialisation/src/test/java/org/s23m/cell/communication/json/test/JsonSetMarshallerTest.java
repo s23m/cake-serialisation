7a5bb4d019f31b5135b7f94fd722b2c69ec3f6fc
Andrew Shewring <andrew.shewring@s23m.com>
2018-01-26 17:23:32 +1300

    Added some more debugging

--- org.s23m.cell.serialisation/src/test/java/org/s23m/cell/communication/json/test/JsonSetMarshallerTest.java
+++ org.s23m.cell.serialisation/src/test/java/org/s23m/cell/communication/json/test/JsonSetMarshallerTest.java
@@ -3,8 +3,8 @@ package org.s23m.cell.communication.json.test;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Assert;
 import org.s23m.cell.Set;
+import org.s23m.cell.api.models.SemanticDomain;
 import org.s23m.cell.communication.AgencyTestFoundationTestCase;
 import org.s23m.cell.communication.SetMarshallingException;
 import org.s23m.cell.communication.json.JsonSetMarshaller;
@@ -12,6 +12,8 @@ import org.s23m.cell.communication.xml.NamespaceConstants;
 import org.s23m.cell.communication.xml.XmlSchemaTerminology;
 import org.s23m.cell.communication.xml.model.dom.Namespace;
 import org.s23m.cell.communication.xml.test.DefaultXmlSchemaTerminology;
+import org.s23m.cell.core.Vertex;
+import org.s23m.cell.platform.api.Instantiation;
 import org.s23m.cell.platform.testfoundation.AgencyTestFoundation;
 
 public class JsonSetMarshallerTest extends AgencyTestFoundationTestCase {
@@ -28,18 +30,28 @@ public class JsonSetMarshallerTest extends AgencyTestFoundationTestCase {
 	public void testSerialise() throws SetMarshallingException {
 		// should be able to convert the set of models and the set of semantic domains to the XML format
 		
+		Namespace namespace = NamespaceConstants.NS_S23M;
+		XmlSchemaTerminology terminology = DefaultXmlSchemaTerminology.getInstance();
+		JsonSetMarshaller setMarshaller = new JsonSetMarshaller(namespace, terminology);
 		for (Set exampleModel : exampleModels) {
-			Namespace namespace = NamespaceConstants.NS_S23M;
-			XmlSchemaTerminology terminology = DefaultXmlSchemaTerminology.getInstance();
-			JsonSetMarshaller setMarshaller = new JsonSetMarshaller(namespace, terminology);
 			
 			String serialised = setMarshaller.serialise(exampleModel);
-			Assert.assertNotNull(serialised);
-			//System.out.println("serialised: " + serialised);
-			
-			// String serialised = xmlSetMarshaller.serialise(Instantiation.toSemanticDomain(ithanku));
-			// ALTERNATIVE EXAMPLE : String serialised = xmlSetMarshaller.serialise(Instantiation.toSemanticDomain(ernst));
-			//System.out.println("serialised: " + serialised);
+			assertNotNull(serialised);
+			System.out.println("serialised: " + serialised);
 		}	
+		
+		Set semanticDomain = Instantiation.toSemanticDomain(AgencyTestFoundation.ithanku);
+		String serialisedSemanticDomain = setMarshaller.serialise(semanticDomain);
+		System.out.println("serialisedSemanticDomain: " + serialisedSemanticDomain);
+		
+		Set containedVertices = semanticDomain.filterProperClass(Vertex.vertex);
+		// try to list all semantic identities
+		Set firstContainedSemanticDomain = containedVertices.extractFirst();
+		final Set orderedSetOfSemanticIdentities = firstContainedSemanticDomain.filterPolymorphic(SemanticDomain.semanticIdentity);
+		for (Set semanticIdentity: orderedSetOfSemanticIdentities) {
+			System.out.println("semanticIdentity: " + semanticIdentity);
+		}
+		
+
 	}
 }
